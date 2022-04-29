1621e4640d067ad6655dbcfd7699a54f2473b92d
Javohirbek009 <104622975+Javohirbek009@users.noreply.github.com>
2022-04-29 12:11:20 +0300

    Add files via upload

---
+++ python 2/python.py
@@ -0,0 +1,46 @@
+
+
+# name = 'javohir'
+# last_name = "ne'matov"
+# game = 'footbal'
+
+# result  = 'salom {} {} pythonni organ {}'.format(name, last_name, game)
+# print(result)
+
+# result  = f"salom {name} {last_name} {game} o'yna"
+# print(result)
+
+
+
+foo = ('html', 'css', 'php', 'javascript', 'c++', 'bootstrap', 'java', 'pascal', 'python', 'ruby')
+value = input('Dasturlash tilini kiriting: ')
+
+if value == 'html':
+    print(foo.index('html'))
+
+elif value == 'css':
+    print(foo.index('css'))
+    
+elif value == 'php':
+    print(foo.index('php'))
+    
+elif value == 'javascript':
+    print(foo.index('javascript'))
+    
+elif value == 'c++':
+    print(foo.index('c++'))
+
+elif value == 'bootstrap':
+    print(foo.index('bootstrap'))
+    
+elif value == 'java':
+    print(foo.index('java'))
+
+elif value == 'pascal':
+    print(foo.index('pascal'))
+    
+elif value == 'python':
+    print(foo.index('python'))
+    
+elif value == 'ruby':
+    print(foo.index('ruby'))
